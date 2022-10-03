e5edfde7c5f6372676bfea4c80128904e405fad8
Ildiko Soltesz <Ildikoosmile@gmail.com>
2022-10-03 20:18:14 +0200

    adding fen (not done yet)

--- main.py
+++ main.py
@@ -1,25 +1,61 @@
+from kivy.config import Config
 from kivy.app import App
 from kivy.graphics import Rectangle, Color
-from kivy.uix.gridlayout import GridLayout
 from kivy.uix.widget import Widget
 
+Config.set('graphics', 'width', '560')
+Config.set('graphics', 'height', '560')
+Config.set('graphics', 'resizable', '0')
 
-class ChessBoard(GridLayout):
-    board = [[0 if (x + y) % 2 == 0 else 1 for x in range(8)] for y in range(8)]
+
+class Piece:
+
+    def __init__(self):
+        self.color = None
+
+    def setColor(self, color):
+        self.color = color
+
+    #def position(self, x, y):
+    #    self.x = x
+    #    self.y = y
+
+
+initialFEN = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
+
+
+def positionsFromFEN(fen_string):
+    board = []
+    for char in fen_string.split()[0]:
+        if char == '/':
+            pass
+        elif char.isdigit():
+            board.append('-' * int(char))
+        else:
+            board.append(char)
+
     print(board)
+
+
+class ChessBoard(Widget):
+    #board = [[0 if (x + y) % 2 == 0 else 1 for x in range(8)] for y in range(8)]
+
+    positionsFromFEN(initialFEN)
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.cols = 8
-        self.rows = 8
+        cols = 8
+        rows = 8
+        tileSize = 70
 
-        for i in range(8):
-            for j in range(8):
-                if self.board[i][j] == 1:
-                    self.canvas.add(Color(0.43, 0.26, 0))
-                else:
-                    self.canvas.add(Color(0.92, 0.85, 0.72))
-                self.canvas.add(Rectangle(pos=((70*i)+100, (70*j)+20), size=(70, 70)))
+        with self.canvas:
+            for i in range(cols):
+                for j in range(rows):
+                    if (i + j) % 2 == 0:
+                        Color(0.43, 0.26, 0)
+                    else:
+                        Color(0.92, 0.85, 0.72)
+                    Rectangle(pos=(tileSize * i, tileSize * j), size=(tileSize, tileSize))
 
 
 class ChessApp(App):
