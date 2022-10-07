e5c53dc85c514efb4690c0807acb844e88a07d95
Ildiko Soltesz <Ildikoosmile@gmail.com>
2022-10-07 14:30:31 +0200

    reformating logic of board + adding pieces labels

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from kivy.config import Config
 from kivy.app import App
 from kivy.graphics import Rectangle, Color
+from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
 from kivy.uix.widget import Widget
 
 Config.set('graphics', 'width', '560')
 Config.set('graphics', 'height', '560')
-Config.set('graphics', 'resizable', '0')
+#Config.set('graphics', 'resizable', '0')
 
 
 class Piece:
@@ -21,46 +24,54 @@ class Piece:
     #    self.y = y
 
 
-initialFEN = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
-
-
 def positionsFromFEN(fen_string):
     board = []
+    innerBoard = []
     for char in fen_string.split()[0]:
         if char == '/':
-            pass
+            board.append(innerBoard)
+            innerBoard = []
         elif char.isdigit():
-            board.append('-' * int(char))
+            innerBoard.extend('-' * int(char))
         else:
-            board.append(char)
+            innerBoard.append(char)
 
     print(board)
+    return board
 
+class DarkTile(Widget):
+    pass
 
-class ChessBoard(Widget):
+class LightTile(Widget):
+    pass
+
+class ChessBoard(GridLayout):
+    layout = GridLayout(cols=8)
     #board = [[0 if (x + y) % 2 == 0 else 1 for x in range(8)] for y in range(8)]
-
-    positionsFromFEN(initialFEN)
+    initialFEN = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR/ w KQkq - 0 1'
+    board = positionsFromFEN(initialFEN)
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        cols = 8
-        rows = 8
-        tileSize = 70
+        cols, rows = 8, 8
 
-        with self.canvas:
-            for i in range(cols):
-                for j in range(rows):
-                    if (i + j) % 2 == 0:
-                        Color(0.43, 0.26, 0)
-                    else:
-                        Color(0.92, 0.85, 0.72)
-                    Rectangle(pos=(tileSize * i, tileSize * j), size=(tileSize, tileSize))
+        for i in range(cols):
+            for j in range(rows):
+                #if (i + j) % 2 == 0:
+                #    self.layout.add_widget(LightTile())
+                #else:
+                #    self.layout.add_widget(DarkTile())
+                #Rectangle(pos=(tileSize * i, tileSize * j), size=(tileSize, tileSize))
+                if self.board[i][j] != '-':
+                    self.layout.add_widget(Label(text=self.board[i][j]))
+                else:
+                    self.layout.add_widget(Label())
+
 
 
 class ChessApp(App):
     def build(self):
-        return ChessBoard()
+        return ChessBoard().layout
 
 
 if __name__ == "__main__":
