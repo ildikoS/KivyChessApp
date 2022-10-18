6fc19cd478637ffc7322c160c5098b1a3df8cd0f
Ildiko Soltesz <Ildikoosmile@gmail.com>
2022-10-18 22:19:19 +0200

    refactor, calculate positions for drag n drop, image added

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from kivy.config import Config
 from kivy.app import App
-from kivy.graphics import Rectangle, Color, Callback
+from kivy.graphics import Rectangle, Color, Line
 from kivy.uix.behaviors import DragBehavior
-from kivy.uix.button import Button
-from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.image import Image
 from kivy.uix.label import Label
 from kivy.uix.widget import Widget
 
@@ -12,9 +11,9 @@ Config.set('graphics', 'width', '560')
 Config.set('graphics', 'height', '560')
 #Config.set('graphics', 'resizable', '0')
 
+tile_size = 70
 
 class Piece:
-
     def __init__(self):
         self.color = None
 
@@ -26,10 +25,28 @@ class Piece:
     #    self.y = y
 
 
-def positionsFromFEN(fen_string):
+class DragPiece(DragBehavior, Image):
+    def on_touch_up(self, touch):
+        super(DragPiece, self).on_touch_up(touch)
+
+        tx, ty = round(self.get_center_x()), round(self.get_center_y())
+        print(tx)
+        print(ty)
+
+        # if tx > ...
+        self.set_center_x(round(tx / tile_size) * tile_size - (tile_size // 2))
+
+        # if ty > ...
+        self.set_center_y(round(ty / tile_size) * tile_size - (tile_size // 2))
+
+    def on_touch_move(self, touch):
+        super(DragPiece, self).on_touch_move(touch)
+
+
+def positions_from_FEN(fenStr):
     board = []
     innerBoard = []
-    for char in fen_string.split()[0]:
+    for char in fenStr.split()[0]:
         if char == '/':
             board.append(innerBoard)
             innerBoard = []
@@ -41,61 +58,35 @@ def positionsFromFEN(fen_string):
     print(board)
     return board
 
-class DarkTile(Widget):
-    pass
-
-class LightTile(Widget):
-    pass
-
-class DragLabel(DragBehavior, Label):
-    def on_touch_up(self, touch):
-        super(DragLabel, self).on_touch_up(touch)
-
-        print("touch up")
-
-#class ChessBoard(FloatLayout):
-#    layout = GridLayout(cols=8, rows=8)
-#    layout2 = GridLayout(cols=8, rows=8)
-#    #board = [[0 if (x + y) % 2 == 0 else 1 for x in range(8)] for y in range(8)]
-#    initialFEN = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR/ w KQkq - 0 1'
-#    board = positionsFromFEN(initialFEN)
-#
-#    def __init__(self, **kwargs):
-#        super().__init__(**kwargs)
-#        cols, rows = 8, 8
-#
-#        for i in range(cols):
-#            for j in range(rows):
-#                if (i + j) % 2 == 0:
-#                    self.layout.add_widget(LightTile())
-#                else:
-#                    self.layout.add_widget(DarkTile())
-#                #Rectangle(pos=(tileSize * i, tileSize * j), size=(tileSize, tileSize))
-#                if self.board[i][j] != '-':
-#                    self.layout2.add_widget(DragLabel(text=self.board[i][j]))
-#                else:
-#                    self.layout2.add_widget(Label())
 
 class ChessBoard(GridLayout):
     initialFEN = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR/ w KQkq - 0 1'
-    board = positionsFromFEN(initialFEN)
+    board = positions_from_FEN(initialFEN)
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.cols, self.rows = 8, 8
 
-        #for i in range(8):
-        #    for j in range(8):
-        #        self.add_widget(DarkTile(pos=(70*i, 70*j))
-        #                        if (i + j) % 2 == 0 else LightTile(pos=(70*i, 70*j))) #pos=(70*i, 70*j)
+        """adding red lines"""
+        screen_height = 70*8
+        for i in range(screen_height//70):
+            with self.canvas:
+                Color(1, .1, .1, .9)
+                Line(width=2, points=[i*70, 0, i*70, screen_height])
+                Line(width=2, points=[0, i*70, screen_height, i*70])
+
+        """drawing board with pieces"""
         for i in range(8):
             for j in range(8):
                 with self.canvas.before:
                     Color(0.92, 0.85, 0.72) if (i + j) % 2 == 0 else Color(0.43, 0.26, 0)
-                    Rectangle(pos=(70*i, 70*j), size=(self.width, self.height))
+                    Rectangle(pos=(70*i, 70*j))
 
-                self.add_widget(DragLabel(text=self.board[i][j])
-                                if self.board[i][j] != '-' else Label())
+                pieceColor = 'w' if self.board[i][j].islower() else 'b'
+                piece = 'king'
+                src = f'NoShadow/128h/{pieceColor}_{piece}_png_128px.png'
+                self.add_widget(DragPiece(source=src) if self.board[i][j] != '-' else Label())
+        #self.add_widget(DragLabel(source='NoShadow/128h/b_king_png_128px.png'))
 
 
 class ChessApp(App):
