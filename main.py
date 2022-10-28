86fc1108b45258e131ad311b5d7c49b0992d1883
Ildiko Soltesz <Ildikoosmile@gmail.com>
2022-10-28 10:19:56 +0200

    add players

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
 from kivy.config import Config
 from kivy.app import App
-from kivy.graphics import Rectangle, Color, Line
 from kivy.uix.behaviors import DragBehavior
 from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.gridlayout import GridLayout
 from kivy.uix.image import Image
-from kivy.uix.label import Label
-from kivy.uix.widget import Widget
 
 Config.set('graphics', 'width', '560')
 Config.set('graphics', 'height', '560')
-#Config.set('graphics', 'resizable', '0')
+Config.set('graphics', 'resizable', '0')
 
 tile_size = 70
 layout = FloatLayout()
@@ -26,13 +22,23 @@ class Piece:
     #    self.x = x
     #    self.y = y
 
-pieces = list()
+class Player:
+    def __init__(self, pieces):
+        self.pieces = pieces
+
+
 blacks = []
 whites = []
+pieces = list()
+player1 = Player(blacks)
+player2 = Player(whites)
+enemy = player1
+
 
 class DragPiece(DragBehavior, Image):
     def __init__(self, **kwargs):
         super(DragPiece, self).__init__(**kwargs)
+        self.downX, self.downY = None, None
 
     def __str__(self):
         return self.source
@@ -41,18 +47,28 @@ class DragPiece(DragBehavior, Image):
         super(DragPiece, self).on_touch_up(touch)
 
         tx, ty = round(self.get_center_x()), round(self.get_center_y())
-        self.set_center_x((tx // tile_size) * tile_size + (tile_size // 2))
-        self.set_center_y((ty // tile_size) * tile_size + (tile_size // 2))
+        centerX = (tx // tile_size) * tile_size + (tile_size // 2)
+        centerY = (ty // tile_size) * tile_size + (tile_size // 2)
+        self.set_center_x(centerX)
+        self.set_center_y(centerY)
 
-        for idx, piece in enumerate(whites):
+        for idx, piece in enumerate(enemy.pieces):
             if self.collide_point(*touch.pos) and self.collide_point(piece.get_center_x(), piece.get_center_y()):
                 print(f"{piece} was removed")
                 print("---------")
-                #whites.remove(piece)
+                enemy.pieces.remove(piece)
                 layout.remove_widget(piece)
 
-    def on_touch_move(self, touch):
-        super(DragPiece, self).on_touch_move(touch)
+        if self.collide_point(*touch.pos):
+            if centerX != self.downX or centerY != self.downY:
+                print("moved away from prev pos")
+                #enemy = player1 if enemy == player2 else player2
+            print(f"centerX={centerX} and downX={self.downX}")
+            print(f"centerY={centerY} and downY={self.downY}")
+
+    def on_touch_down(self, touch):
+        super(DragPiece, self).on_touch_down(touch)
+        self.downX, self.downY = round(self.get_center_x()), round(self.get_center_y())
 
 
 def positions_from_FEN(fenStr):
@@ -87,7 +103,6 @@ class ChessBoard():
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.layout = layout
-        #layout.cols, layout.rows = 8, 8
 
         """drawing board with pieces"""
         for i in range(8):
@@ -96,10 +111,6 @@ class ChessBoard():
                 layout.add_widget(Image(source=f'128h/{color}_png_128px.png',
                                         pos=(tile_size*j, tile_size*i),
                                         size_hint=(0.125, 0.125)))
-                #with layout.canvas.before:
-                #    Color(0.92, 0.85, 0.72) if (i + j) % 2 == 0 else Color(0.43, 0.26, 0)
-                #    Rectangle(pos=(tile_size*i, tile_size*j))
-
         self.drawPieces()
 
     def drawPieces(self):
@@ -114,8 +125,6 @@ class ChessBoard():
                     src = f'128h/{pieceColor}_{piece}_png_128px.png'
                     pieceInstance = DragPiece(source=src, pos=(tile_size*j, tile_size*i))
                     layout.add_widget(pieceInstance)
-                else:
-                    layout.add_widget(Label())
 
                 if pieceColor == 'b':
                     blacks.append(pieceInstance)
@@ -123,7 +132,6 @@ class ChessBoard():
                     whites.append(pieceInstance)
 
                 pieces.append(pieceInstance)
-        #self.add_widget(DragLabel(source='NoShadow/128h/b_king_png_128px.png'))
 
 
 class ChessApp(App):
