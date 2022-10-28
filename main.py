13da35968f767221984dc96be6fb192ceb5d49ab
Ildiko Soltesz <Ildikoosmile@gmail.com>
2022-10-28 17:26:25 +0200

    refactoring + adding piece classes

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from kivy.uix.behaviors import DragBehavior
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.image import Image
 
+import piece
+
 Config.set('graphics', 'width', '560')
 Config.set('graphics', 'height', '560')
 Config.set('graphics', 'resizable', '0')
@@ -11,16 +13,6 @@ Config.set('graphics', 'resizable', '0')
 tile_size = 70
 layout = FloatLayout()
 
-class Piece:
-    def __init__(self):
-        self.color = None
-
-    def setColor(self, color):
-        self.color = color
-
-    #def position(self, x, y):
-    #    self.x = x
-    #    self.y = y
 
 class Player:
     def __init__(self, pieces):
@@ -33,42 +25,6 @@ pieces = list()
 player1 = Player(blacks)
 player2 = Player(whites)
 enemy = player1
-
-
-class DragPiece(DragBehavior, Image):
-    def __init__(self, **kwargs):
-        super(DragPiece, self).__init__(**kwargs)
-        self.downX, self.downY = None, None
-
-    def __str__(self):
-        return self.source
-
-    def on_touch_up(self, touch):
-        super(DragPiece, self).on_touch_up(touch)
-
-        tx, ty = round(self.get_center_x()), round(self.get_center_y())
-        centerX = (tx // tile_size) * tile_size + (tile_size // 2)
-        centerY = (ty // tile_size) * tile_size + (tile_size // 2)
-        self.set_center_x(centerX)
-        self.set_center_y(centerY)
-
-        for idx, piece in enumerate(enemy.pieces):
-            if self.collide_point(*touch.pos) and self.collide_point(piece.get_center_x(), piece.get_center_y()):
-                print(f"{piece} was removed")
-                print("---------")
-                enemy.pieces.remove(piece)
-                layout.remove_widget(piece)
-
-        if self.collide_point(*touch.pos):
-            if centerX != self.downX or centerY != self.downY:
-                print("moved away from prev pos")
-                #enemy = player1 if enemy == player2 else player2
-            print(f"centerX={centerX} and downX={self.downX}")
-            print(f"centerY={centerY} and downY={self.downY}")
-
-    def on_touch_down(self, touch):
-        super(DragPiece, self).on_touch_down(touch)
-        self.downX, self.downY = round(self.get_center_x()), round(self.get_center_y())
 
 
 def positions_from_FEN(fenStr):
@@ -87,16 +43,17 @@ def positions_from_FEN(fenStr):
     return board
 
 
-piecesDict = {
-    'k': "king", #King()
-    'q': "queen",
-    'b': "bishop",
-    'n': "knight",
-    'r': "rook",
-    'p': "pawn"
-}
+#piecesDict = {
+#    'k': King(),
+#    'q': Queen(),
+#    'b': Bishop(),
+#    'n': Knight(),
+#    'r': Rook(),
+#    'p': Pawn()
+#}
 
-class ChessBoard():
+
+class ChessBoard:
     initialFEN = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR/ w KQkq - 0 1'
     board = positions_from_FEN(initialFEN)
 
@@ -108,30 +65,39 @@ class ChessBoard():
         for i in range(8):
             for j in range(8):
                 color = 'square brown light' if (i + j) % 2 == 0 else 'square brown dark'
-                layout.add_widget(Image(source=f'128h/{color}_png_128px.png',
-                                        pos=(tile_size*j, tile_size*i),
+                layout.add_widget(Image(source=f'128h/{color}_png_128px.png', pos=(tile_size*j, tile_size*i),
                                         size_hint=(0.125, 0.125)))
-        self.drawPieces()
+        self.draw_pieces()
 
-    def drawPieces(self):
+    def draw_pieces(self):
         for i in range(8):
             for j in range(8):
-                pieceColor = 'b'
-                if self.board[i][j].islower():
-                    pieceColor = 'w'
-                piece = piecesDict.get(self.board[i][j].lower())
+                filePiece = self.board[i][j]
 
-                if self.board[i][j] != '-':
-                    src = f'128h/{pieceColor}_{piece}_png_128px.png'
-                    pieceInstance = DragPiece(source=src, pos=(tile_size*j, tile_size*i))
-                    layout.add_widget(pieceInstance)
+                if filePiece != '-':
+                    currPiece = self.get_piece(filePiece.lower())
 
-                if pieceColor == 'b':
-                    blacks.append(pieceInstance)
-                else:
-                    whites.append(pieceInstance)
+                    #currPiece = piecesDict[filePiece.lower()]
+                    pieceColor = 'w' if filePiece.islower() else 'b'
+#
+                    srcImg = f'128h/{pieceColor}_{currPiece}_png_128px.png'
+                    #pieceInstance = currPiece(source=srcImg, pos=(tile_size*j, tile_size*i))
+                    currPiece.source = srcImg
+                    currPiece.pos = (tile_size * j, tile_size * i)
+                    currPiece.size_hint = (0.125, 0.125)
+                    layout.add_widget(currPiece)
 
-                pieces.append(pieceInstance)
+                    blacks.append(currPiece) if pieceColor == 'b' else whites.append(currPiece)
+                    pieces.append(currPiece)
+        print(blacks)
+
+    def get_piece(self, char):
+        if char == 'k': return piece.King()
+        if char == 'q': return piece.Queen()
+        if char == 'b': return piece.Bishop()
+        if char == 'n': return piece.Knight()
+        if char == 'r': return piece.Rook()
+        if char == 'p': return piece.Pawn()
 
 
 class ChessApp(App):
---
+++ piece.py
@@ -0,0 +1,80 @@
+from kivy.uix.behaviors import DragBehavior
+from kivy.uix.image import Image
+
+from main import tile_size, layout, enemy
+
+
+class DragPiece(DragBehavior, Image):
+    def __init__(self, **kwargs):
+        super(DragPiece, self).__init__(**kwargs)
+        self.downX, self.downY = None, None
+
+    def on_touch_up(self, touch):
+        super(DragPiece, self).on_touch_up(touch)
+
+        tx, ty = round(self.get_center_x()), round(self.get_center_y())
+        centerX = (tx // tile_size) * tile_size + (tile_size // 2)
+        centerY = (ty // tile_size) * tile_size + (tile_size // 2)
+        self.set_center_x(centerX)
+        self.set_center_y(centerY)
+
+        for idx, piece in enumerate(enemy.pieces):
+            if self.collide_point(*touch.pos) and self.collide_point(piece.get_center_x(), piece.get_center_y()):
+                print(f"{piece} was removed")
+                print("---------")
+                enemy.pieces.remove(piece)
+                layout.remove_widget(piece)
+
+        if self.collide_point(*touch.pos):
+            if centerX != self.downX or centerY != self.downY:
+                print("moved away from prev pos")
+                #enemy = player1 if enemy == player2 else player2
+            print(f"centerX={centerX} and downX={self.downX}")
+            print(f"centerY={centerY} and downY={self.downY}")
+
+    def on_touch_down(self, touch):
+        super(DragPiece, self).on_touch_down(touch)
+
+        self.downX, self.downY = round(self.get_center_x()), round(self.get_center_y())
+
+class Piece(Image):
+    def __init__(self, **kwargs):
+        super(Piece, self).__init__(**kwargs)
+        #self.color = None
+
+    #def setColor(self, color):
+    #    self.color = color
+
+    #def position(self, x, y):
+    #    self.x = x
+    #    self.y = y
+
+
+class King(DragPiece):
+    def __str__(self):
+        return "king"
+
+
+class Queen(DragPiece):
+    def __str__(self):
+        return "queen"
+
+
+class Bishop(DragPiece):
+    def __str__(self):
+        return "bishop"
+
+
+class Knight(DragPiece):
+    def __str__(self):
+        return "knight"
+
+
+class Rook(DragPiece):
+    def __str__(self):
+        return "rook"
+
+
+class Pawn(DragPiece):
+    def __str__(self):
+        return "pawn"
